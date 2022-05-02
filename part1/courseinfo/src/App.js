212cdb8a2f32169f2f42477a34ab101ac4cf6c58
Cryocooler <sahin.dursun@icloud.com>
2022-05-02 19:52:04 +0300

    courseinfo 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,18 +1,22 @@
-const Header = (props) => {
+const Header = ({course}) => {
+  const {id, name, parts} = course
+  console.log('header', name)
   return (
     
-      <h1>{props.course}</h1>
+      <h1>{name}</h1>
     
   )
 }
 
-const Content = (props) => {
-  console.log(props);
+const Content = (course) => {
+  const parts = course.parts.parts
+  console.log('Content props', parts);
   return (
     <div>
-      <Part part = {props.parts.part1}/>
-      <Part part = {props.parts.part2}/>
-      <Part part = {props.parts.part3}/>
+      
+      <Part part = {parts[0]}/>
+      <Part part = {parts[1]}/>
+      <Part part = {parts[2]}/>
      
   
     </div>
@@ -21,48 +25,54 @@ const Content = (props) => {
 
 }
 
-const Part = (props) => {
-  console.log('props passed to part');
-  const name = props.part.name
-  const exercises = props.part.exercises
+const Part = ({part}) => {
+  console.log('props passed to part', part);
+  const {name, exercises} = part
+  console.log(name)
+ 
   return (
     
     <p>{name} {exercises}</p>
   )
 }
  
-const Total = (parts) => {
+const Total = (course) => {
+  const parts = course.parts.parts
   console.log('parts', parts)
-  const exSum = parts.exercises.part1.exercises + parts.exercises.part2.exercises 
-    + parts.exercises.part3.exercises
+
   
   return (
-    <p>number of exercises {exSum} </p>
+    <p> total of {(parts.map(part => part.exercises)).reduce((x,y) => x +y, 0)} exercises</p>
   )
   
 }
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+
 
   return (
     <div>
       <Header course = {course} />
-      <Content parts = {{part1, part2, part3}} />
-      <Total exercises = {{part1, part2, part3}} />
+      <Content parts = {course} />
+      <Total parts = {course} />
     </div>
   )
 }
