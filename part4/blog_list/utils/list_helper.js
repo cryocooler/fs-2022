2589e577e26405760693391255632d9b0ee8628e
Cryocooler <sahin.dursun@icloud.com>
2022-05-14 14:57:57 +0300

    4.7 helper functions

---
+++ part4/blog_list/utils/list_helper.js
@@ -0,0 +1,48 @@
+const { count } = require("../models/blog");
+
+_ = require("lodash");
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((x, y) => x + y.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const mostLiked = blogs.reduce(
+    (x, y) => (x.likes > y.likes ? x : y),
+    blogs,
+    null
+  );
+  return mostLiked;
+};
+
+const mostBlogs = (blogs) => {
+  var freq = _.countBy(blogs, "author");
+  var most = _.maxBy(_.entries(freq));
+
+  return { author: most[0], blogs: most[1] };
+};
+
+const mostLikes = (blogs) => {
+  var most = _.chain(blogs)
+    .groupBy("author")
+    .map((blog, author) => ({
+      author: author,
+      likes: _.sumBy(blog, "likes"),
+    }))
+    .maxBy("likes")
+    .value();
+
+  return most;
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
