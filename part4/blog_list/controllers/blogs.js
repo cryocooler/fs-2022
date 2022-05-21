ea96a6a4acbe9ec687ddbb32c237b288467beae4
Cryocooler <sahin.dursun@icloud.com>
2022-05-21 14:59:03 +0300

    4.14 blog list expansions, step2

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -6,15 +6,30 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs);
 });
 
+blogsRouter.get("/:id", async (request, response) => {
+  Blog.findById(request.params.id);
+  response.json();
+});
+
 blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
 
-  try {
-    const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
-  } catch {
-    exception;
-  }
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body;
+  const blog = {
+    likes: body.likes,
+  };
+  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+  response.status(204).end();
 });
 
 //   blog.save().then((result) => {
--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -4,72 +4,109 @@ const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
 const listHelper = require("../utils/list_helper");
-
-const initialBlogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0,
-  },
-];
+const helper = require("../tests/test_helper");
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  let blogObject = new Blog(initialBlogs[0]);
+  let blogObject = new Blog(helper.initialBlogs[0]);
   await blogObject.save();
-  blogObject = new Blog(initialBlogs[1]);
+  blogObject = new Blog(helper.initialBlogs[1]);
   await blogObject.save();
 });
+describe("tests for get and post end points", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  }, 10000);
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-}, 10000);
+  test("there are two blogs", async () => {
+    const response = await api.get("/api/blogs");
 
-test("there are two blogs", async () => {
-  const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(2);
+  });
 
-  expect(response.body).toHaveLength(2);
+  test("the first blog in the db is the first blog in the array", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body[0].author).toBe("Michael Chan");
+  });
+
+  test("unique identifier is ID", async () => {
+    const response = await api.get("/api/blogs");
+    //console.log("response IS", Object.keys(response.body[0]));
+    expect(Object.keys(response.body[0])[4]).toBe("id");
+  });
+
+  test("a new blog can be succesfully created", async () => {
+    const newBlog = {
+      title: "tickle your fancy",
+      author: "Sara vanninen",
+      url: "https://tickleyourfancy.blogspot.com",
+      likes: 50000,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(201);
+
+    const response = await api.get("/api/blogs");
+    const count = response.body.map((r) => r.id);
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1);
+  });
+
+  test("undefined likes defaults to 0", async () => {
+    const newBlog = {
+      title: "tickle your fancy",
+      author: "Sara vanninen",
+      url: "https://tickleyourfancy.blogspot.com",
+    };
+
+    await await api.post("/api/blogs").send(newBlog).expect(201);
+
+    const res = await api.get("/api/blogs");
+    const length = res.body.length;
+    //console.log("lenght", length);
+    const likes = res.body[length - 1].likes;
+    //console.log("LIKES", likes);
+    expect(likes).toBe(0);
+  });
+
+  test("missing title and url returns bad request", async () => {
+    const newBlog = {
+      title: "tickle your fancy",
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+  }, 10000);
 });
 
-test("the first blog is authored by Michael Chan", async () => {
-  const response = await api.get("/api/blogs");
+describe("tests for delete endpoint", () => {
+  test("succeeds with status code 204 if id is valid", async () => {
+    const dbBlogs = await helper.blogsInDb();
+    console.log(dbBlogs);
 
-  expect(response.body[0].author).toBe("Michael Chan");
+    const blogToDelete = dbBlogs[0];
+    console.log("blog id for deletion", blogToDelete);
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+    const blogsAfterDelete = await helper.blogsInDb();
+    expect(blogsAfterDelete).toHaveLength(helper.initialBlogs.length - 1);
+  });
+  test("fails with 204 if id is invalid", async () => {
+    const badId = "axadasdasdas";
+    await api.delete(`/api/blogs/${badId}`).expect(204);
+  });
 });
-
-test("unique identifier is ID", async () => {
-  const response = await api.get("/api/blogs");
-  //console.log("response IS", Object.keys(response.body[0]));
-  expect(Object.keys(response.body[0])[4]).toBe("id");
-});
-
-test("A new blog can be succesfully created", async () => {
-  const newBlog = {
-    title: "tickle your fancy",
-    author: "Sara vanninen",
-    url: "https://tickleyourfancy.blogspot.com",
-    likes: 50000,
-  };
-
-  await api.post("/api/blogs").send(newBlog).expect(201);
-
-  const response = await api.get("/api/blogs");
-  const count = response.body.map((r) => r.id);
-  expect(response.body).toHaveLength(initialBlogs.length + 1);
+describe("updating blog information", () => {
+  test("blog likes can be updated", async () => {
+    const blog = {
+      likes: 100,
+    };
+    const dbBlogs = await helper.blogsInDb();
+    const blogToUpdate = dbBlogs[0];
+    await api.put(`/api/blogs/${blogToUpdate.id}`).send(blog).expect(204);
+    const updatedBlog = await helper.blogsInDb();
+    expect(updatedBlog[0].likes).toBe(blog.likes);
+  });
 });
 
 afterAll(() => {
