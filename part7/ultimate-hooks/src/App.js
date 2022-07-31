4f15ed7093989a3ae65089f9c72adf6e77b89510
Cryocooler <sahin.dursun@icloud.com>
2022-07-31 18:09:05 +0300

    7.8 ultimate hooks

---
+++ part7/ultimate-hooks/src/App.js
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([]);
+  // console.log("pre-hook resources", resources);
+
+  // ...
+  let token = null;
+
+  const setToken = (newToken) => {
+    token = `bearer ${newToken}`;
+  };
+
+  const getAllHook = () => {
+    console.log("hook fired");
+    axios
+      .get(baseUrl)
+      .then((response) => {
+        setResources(response.data);
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+
+    //console.log("RESOURE", resources);
+  };
+
+  useEffect(getAllHook, []);
+
+  const create = (resource) => {
+    // ...
+    const config = { headers: { Authorization: token } };
+    console.log("creation hook fired");
+    axios
+      .post(baseUrl, resource, config)
+      .then((response) => {
+        setResources(resources.concat(response.data));
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+    //console.log("CREATION RESOURCE", resources);
+  };
+
+  const service = {
+    create,
+  };
+
+  return [resources, service];
+};
+
+const App = () => {
+  const content = useField("text");
+  const name = useField("text");
+  const number = useField("text");
+
+  const [notes, noteService] = useResource("http://localhost:3005/notes");
+  const [persons, personService] = useResource("http://localhost:3005/persons");
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault();
+    noteService.create({ content: content.value });
+  };
+
+  const handlePersonSubmit = (event) => {
+    event.preventDefault();
+    personService.create({ name: name.value, number: number.value });
+  };
+
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
+
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br />
+        number <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
